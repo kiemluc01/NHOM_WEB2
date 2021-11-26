41ae77e18c6af92c303962cb2acb82c7943cf867
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-11-26 11:49:55 +0700

    set all the icon to link

--- Views/modules/footer.php
+++ Views/modules/footer.php
@@ -11,10 +11,10 @@ Book là một dịch vụ đăng ký đọc kỹ thuật số. Đổi lại v�
 gồm hơn 500.000 cuốn sáchbằng nhiều ngôn ngữ. Đăng ký này có thể được chấm dứt bất kỳ lúc nào trong phần "Đăng ký".
         </pre>
         <div id="icon"  style="padding-top: 2%;">
-            <img src="Public/images/icon_twitter.png" alt="twicter" class="img_icon">
-            <img src="Public/images/icon_fb.png" alt="facebook" class="img_icon" >
-            <img src="Public/images/icon_yt.png" alt="youtube" class="img_icon">
-            <img src="Public/images/icon_instagram.png" alt="intagram" class="img_icon">
+            <a href="#"><img src="Public/images/icon_twitter.png" alt="twicter" class="img_icon"></a>
+            <a href="#"><img src="Public/images/icon_fb.png" alt="facebook" class="img_icon" ></a>
+            <a href="#"><img src="Public/images/icon_yt.png" alt="youtube" class="img_icon"></a>
+            <a href="#"><img src="Public/images/icon_instagram.png" alt="intagram" class="img_icon"></a>
         </div>
     </center>
 
