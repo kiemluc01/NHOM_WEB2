7970de869eec794e52b14f2947dff4fa6a505157
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-12-03 14:07:16 +0700

    add rating

--- Views/components/book/index.php
+++ Views/components/book/index.php
@@ -23,14 +23,21 @@ while ($row = $result->fetch_assoc()) {
             <div id="main_details1">
                 <h2 id="bookname"><?php echo $Tensach; ?></h2>
                 <p id="writer"><?php echo $Tacgia; ?></p>
-                <div class="star-rating">
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
-                    <i class="far fa-star i-rate"></i>
+                <div id="rating">
+                    <input type="radio" id="star5" name="rating" value="5">
+                    <label class="full" for="star5" title="Awesome - 5 stars"></label>
+
+                    <input type="radio" id="star4" name="rating" value="4">
+                    <label class="full" for="star4" title="Pretty good - 4 stars"></label>
+
+                    <input type="radio" id="star3" name="rating" value="3">
+                    <label class="full" for="star3" title="Meh - 3 stars"></label>
+
+                    <input type="radio" id="star2" name="rating" value="2">
+                    <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
+
+                    <input type="radio" id="star1" name="rating" value="1">
+                    <label class="full" for="star1" title="Sucks big time - 1 star"></label>
                 </div>
                 <!-- name book, writer and rate number star -->
             </div>
