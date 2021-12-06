023f52522e7a86b53f4f15b6e3c5cd98558334e8
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-12-06 17:44:28 +0700

    change the footer again :((

--- Views/modules/footer.php
+++ Views/modules/footer.php
@@ -2,6 +2,12 @@
     <div class="footer-item logo">
         <img src="Public\images\logo_light.png" alt="">
     </div>
+    <div class="footer-item slogan">
+        <p><em>Three Owls Book</em> is something ...</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae sunt at reprehenderit
+            voluptates necessitatibus. Natus necessitatibus error cumque maxime.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, temporibus!</p>
+    </div>
     <div class="footer-item contact">
         <h3>Nhóm Three Owls</h3>
         <p>Liên hệ với chúng tôi: </p>
@@ -9,12 +15,6 @@
         <address><a href="mailto:threeowlsbooks@gmail.com">threeowlsbooks@gmail.com</a></address>
         <p>Số điện thoại: </p>
         <a href="tel:6156884511">615-688-4511</a>
-    </div>
-    <div class="footer-item slogan">
-        <p><em>Three Owls Book</em> is something ...</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae sunt at reprehenderit
-            voluptates necessitatibus. Natus necessitatibus error cumque maxime.</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam, temporibus!</p>
     </div>
     <div class="footer-item" id="icon">
         <a href="https://twitter.com/"><i class="fab fa-twitter-square i-link"></i></a><br>
