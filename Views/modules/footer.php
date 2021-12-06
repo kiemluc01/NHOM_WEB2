3d4473405709d9094e51a5a2806ae8a99119ba81
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-12-06 17:15:42 +0700

    change the footer

--- Views/modules/footer.php
+++ Views/modules/footer.php
@@ -1,22 +1,17 @@
-<div id="footer" style="background-color: #35363B; height: 500px;">
-    <center  class="footer" >
-        <h1  class="footer" >Bạn cần giúp đỡ?</h1>
-        <pre class="footer" >Hãy liên hệ với chúng tôi
-book@gmail.com
-        </pre>
-        <pre class="footer" >
-Book là một dịch vụ đăng ký đọc kỹ thuật số. Đổi lại với một khoản phí nhỏ hàng tháng,
- bạn có thể tải xuống và thưởng thức việc đọc từ danh mục sách điện tử hoàn chỉnhcủa chúng tôi trên mọi thiết bị 
-(điện thoại di động, máy tính bảng, thiết bị đọc sách điện tử với trình điều hướng web hoặc PC). Danh mục của chúng tôi bao 
-gồm hơn 500.000 cuốn sáchbằng nhiều ngôn ngữ. Đăng ký này có thể được chấm dứt bất kỳ lúc nào trong phần "Đăng ký".
-        </pre>
-        <div id="icon"  style="padding-top: 2%;">
-            <a href="#"><i class="fab fa-twitter-square i-link"></i></a>
-            <a href="#"><i class="fab fa-facebook-square i-link"></i></a>
-            <a href="#"><i class="fab fa-youtube-square i-link"></i></a>
-            <a href="#"><i class="fab fa-instagram-square i-link"></i></a>
-        </div>
-    </center>
-
+<div class="footer clear-fix">
+    <div class="footer-item logo">
+        <img src="Public\images\logo_light.png" alt="">
+    </div>
+    <div class="footer-item contact">
+        <p>Contact information</p>
+    </div>
+    <div class="footer-item slogan">
+        <p>Three Owls Book is ...</p>
+    </div>
+    <div class="footer-item" id="icon">
+        <a href="https://twitter.com/"><i class="fab fa-twitter-square i-link"></i></a><br>
+        <a href="https://facebook.com/"><i class="fab fa-facebook-square i-link"></i></a><br>
+        <a href="https://www.youtube.com/"><i class="fab fa-youtube-square i-link"></i></a><br>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram-square i-link"></i></a><br>
+    </div>
 </div>
-<script src="Public/javascript/main.js"></script>
