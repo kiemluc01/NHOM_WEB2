289b202356ed430c72f0228e6e8622c5b7080bb9
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-12-29 17:19:32 +0700

    livesearch lam tiep 1/3000 anh yeu em 3000 nam ooo yeah yeah blackpink in your area blink blink :v

--- Public/javascript/searchBook.js
+++ Public/javascript/searchBook.js
@@ -1,16 +1,19 @@
-function showBook(filter) {
+function showBook(filter, inp) {
     if (filter.length == 0) {
         // document.getElementById("liveSearch").innerHTML = "";
-        document.getElementById("liveSearch").style.border = "2px solid red";
+        document.getElementById("liveSearch").style.display = "none";
     }
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState==4 && this.status==200) {
+            document.getElementById("liveSearch").style.display = "block";
             document.getElementById("liveSearch").innerHTML=this.responseText;
             document.getElementById("liveSearch").style.border="2px solid yellow"
         }
     }
     xmlhttp.open("GET", "Views/modules/livesearchbuonba.php?q=" + filter, true);
     xmlhttp.send();
-    // document.getElementById("liveSearch").style.background = "orange";
+}
+function hideliveSearch() {
+    document.getElementById("liveSearch").style.display = "none";
 }
--- Views/modules/searchBook.php
+++ Views/modules/searchBook.php
@@ -1,6 +1,6 @@
 <div class="search-container">
     <form action="" class="find">
-        <input type="text" name="find" id="" placeholder="Tìm kiếm .." class="find" onkeyup="showBook(this.value)">
+        <input type="text" name="find" id="" placeholder="Tìm kiếm .." class="find" onfocus="showBook(this.value)" onkeyup="showBook(this.value)" onblur="hideliveSearch()">
         <div id="liveSearch"></div>
         <button type="submit"><i class="fa fa-search"></i></button>
     </form>
