37702ac7a9763fe0c02fcda6b55fee497f40641b
kiemluc01 <kiemluc01@gmail.com>
2022-01-08 08:44:37 +0700

    fixx

--- Views/components/book/index.php
+++ Views/components/book/index.php
@@ -8,7 +8,6 @@ while ($row = $result->fetch_assoc()) {
     $Tacgia = $row['Tacgia'];
     $nd = $row['TomtatND'];
 }
-
 ?>
 <!-- Phần content -->
 <div class="fluid-container mt-5 mx-3">
@@ -71,16 +70,12 @@ while ($row = $result->fetch_assoc()) {
                     <div id="rating">
                         <input type="radio" id="star5" name="rating" value="5">
                         <label class="full" for="star5" title="Awesome - 5 stars"></label>
-
                         <input type="radio" id="star4" name="rating" value="4">
                         <label class="full" for="star4" title="Pretty good - 4 stars"></label>
-
                         <input type="radio" id="star3" name="rating" value="3">
                         <label class="full" for="star3" title="Meh - 3 stars"></label>
-
                         <input type="radio" id="star2" name="rating" value="2">
                         <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
-
                         <input type="radio" id="star1" name="rating" value="1">
                         <label class="full" for="star1" title="Sucks big time - 1 star"></label>
                     </div>
