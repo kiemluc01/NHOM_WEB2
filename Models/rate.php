6363c9a2474337853b2f1953e4a058616bf96d83
kiemluc01 <kiemluc01@gmail.com>
2022-01-14 11:11:14 +0700

    rate and fvr

---
+++ Models/rate.php
@@ -0,0 +1,32 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "docsach");
+mysqli_query($connect, "SET NAMES 'utf8'");
+
+if (isset($_REQUEST['user']) && isset($_REQUEST['idsach']) && isset($_REQUEST['sosao'])) {
+    $user = $_REQUEST['user'];
+    $idsach = $_REQUEST['idsach'];
+    $sosao = $_REQUEST['sosao'];
+    $sql = "select * from tblaccount where username ='" . $user . "'";
+    $result = mysqli_query($connect, $sql);
+    $iduser = "";
+    while ($row = $result->fetch_assoc()) {
+        $iduser = $row['idMember'];
+    }
+    if (mysqli_query($connect, "select * from rate where idSach=" . $idsach . " and idMember=" . $iduser))
+        $sql = "update rate set sosao=" . $sosao . " where idSach=" . $idsach . " and idMember=" . $iduser;
+    else
+        $sql = "insert into rate values(" . $idsach . "," . $iduser . "," . $sosao . ");";
+    mysqli_query($connect, $sql);
+    $sql = "select idSach,avg(sosao) as sosao from rate where idSach =" . $_REQUEST['idsach'] . " group by idSach";
+    $rate = 0;
+    $result = mysqli_query($connect, $sql);
+    if (
+        $result->num_rows > 0
+    ) {
+        while ($row = $result->fetch_assoc())
+            $rate = $row['sosao'];
+    }
+    echo round($rate, 1) . "★";
+?>
+
+<?php } ?>
--- Views/components/book/index.php
+++ Views/components/book/index.php
@@ -1,11 +1,12 @@
 <?php
 $row = array();
 $book = loadModel('Book');
+$member = loadModel('Member');
 $book->Views();
 $result = $book->get_bookcurrent($_REQUEST['idSach']);
 while ($row = $result->fetch_assoc()) {
     if (substr($row['imgSach'], 0, 4) == "http")
-    $imgSach = $row['imgSach'];
+        $imgSach = $row['imgSach'];
     else
         $imgSach = "admin/" . $row['imgSach'];
     $Tensach = $row['Tensach'];
@@ -71,7 +72,20 @@ while ($row = $result->fetch_assoc()) {
             <div class="row">
                 <form action="#" method="post" class="clear-fix">
                     <div id="rating">
-                        <input type="radio" id="star5" name="rating" value="5">
+                        <?php $sosao = $book->checkRate();
+                        for ($i = 1; $i <= 5; $i++) {
+                            if ($i == $sosao) { ?>
+                                <input type="radio" id="star<?php echo $i ?>" name="rating" value="<?php echo $i ?>" checked>
+                                <label class="full" for="star<?php echo $i ?>" title="Awesome - <?php echo $i ?> stars"></label>
+                            <?php } else {
+                            ?>
+                                <input type="radio" id="star<?php echo $i ?>" name="rating" value="<?php echo $i ?>">
+                                <label class="full" for="star<?php echo $i ?>" title="Awesome - <?php echo $i ?> stars"></label>
+                        <?php
+                            }
+                        }
+                        ?>
+                        <!-- <input type="radio" id="star5" name="rating" value="5">
                         <label class="full" for="star5" title="Awesome - 5 stars"></label>
                         <input type="radio" id="star4" name="rating" value="4">
                         <label class="full" for="star4" title="Pretty good - 4 stars"></label>
@@ -80,11 +94,18 @@ while ($row = $result->fetch_assoc()) {
                         <input type="radio" id="star2" name="rating" value="2">
                         <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
                         <input type="radio" id="star1" name="rating" value="1">
-                        <label class="full" for="star1" title="Sucks big time - 1 star"></label>
+                        <label class="full" for="star1" title="Sucks big time - 1 star"></label> -->
+                        <p id="rate_percent"><?php echo $book->getRate(); ?>★</p>
                     </div>
                     <div id="add-favourite">
-                        <input type="checkbox" name="favourite" id="favour" value="1">
-                        <label for="favour"></label>
+                        <?php if ($book->check_f()) {
+                        ?>
+                            <input type="checkbox" name="favourite" id="favour" value="1" checked>
+                            <label for="favour" id="favourite">Bỏ thích</label> <?php } else { ?>
+                            <input type="checkbox" name="favourite" id="favour" value="1">
+                            <label for="favour" id="favourite">Thích</label>
+                        <?php }
+                        ?>
                         <!-- <span>Thêm vào yêu thích</span>  -->
                     </div>
                 </form>
@@ -105,8 +126,8 @@ while ($row = $result->fetch_assoc()) {
             if ($result->num_rows > 0)
                 while ($row = $result->fetch_assoc()) { ?>
                 <p><a href="<?php if (isset($_REQUEST['condition'])) echo  'index.php?condition=' . $_REQUEST['condition'] . '&option=book&idSach=' . $_REQUEST['idSach'] . '&chapter=' . $row['TenChuong'] . '&page=1&kitu=0'; ?>"><span> <?php echo $row['TenChuong']; ?></span></a></p>
-            <?php   
-            }
+            <?php
+                }
             ?>
         </div>
     </div>
@@ -117,3 +138,128 @@ while ($row = $result->fetch_assoc()) {
         <?php loadModule('new_book'); ?>
     </div>
 </div>
+<input type="text" id="user" style="display:none" value="<?php echo (isset($_SESSION['user']) ? $_SESSION['user'] : ""); ?>">
+<input type="text" id="idSach" style="display:none" value="<?php echo $_REQUEST['idSach']; ?>">
+<script>
+    $(document).ready(function() {
+        $('#favourite').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/favourite.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach
+                    },
+                    success: function(data) {
+                        $("#favourite").html(data);
+                    }
+                })
+            }
+        })
+        $('#star1').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/rate.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach,
+                        sosao: "1",
+                    },
+                    success: function(data) {
+                        $("#rate_percent").html(data);
+                    }
+                })
+            }
+        })
+        $('#star2').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/rate.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach,
+                        sosao: "2",
+                    },
+                    success: function(data) {
+                        $("#rate_percent").html(data);
+                    }
+                })
+            }
+        })
+        $('#star3').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/rate.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach,
+                        sosao: "3",
+                    },
+                    success: function(data) {
+                        $("#rate_percent").html(data);
+                    }
+                })
+            }
+        })
+        $('#star4').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/rate.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach,
+                        sosao: "4",
+                    },
+                    success: function(data) {
+                        $("#rate_percent").html(data);
+                    }
+                })
+            }
+        })
+        $('#star5').click(function() {
+            var user = $('#user').val();
+            var idsach = $('#idSach').val();
+            if (user === "")
+                alert("bạn phải đăng nhập trước")
+            else {
+                $.ajax({
+                    url: 'Models/rate.php',
+                    type: 'POST',
+                    data: {
+                        user: user,
+                        idsach: idsach,
+                        sosao: "5",
+                    },
+                    success: function(data) {
+                        $("#rate_percent").html(data);
+                    }
+                })
+            }
+        })
+    })
+</script>
